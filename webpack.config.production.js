f6122d8a64e2558e91fc55e9736a434546cdda0e
Dizzy <itsdizzy40@gmail.com>
2017-05-17 17:58:00 +0200

    Fixed build script, yus i fked up...

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -16,10 +16,6 @@ module.exports = webpackMerge(baseConfig, {
         chunkFilename: 'js/[id].[hash].chunk.js'
     },
 
-    htmlLoader: {
-        minimize: false // workaround for ng2
-    },
-
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.DedupePlugin(),
